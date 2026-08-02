fd237e238259935338260c3a9d588aef650433eb
Hugo Hurskainen <hugo.hurskainen@gmail.com>
2026-08-02 19:43:21 +0000

    Version 0.9.11

--- src/version.h
+++ src/version.h
@@ -15,7 +15,7 @@
 #ifndef HLD_VERSION_H
 #define HLD_VERSION_H
 
-#define HLD_VERSION "0.9.10"
+#define HLD_VERSION "0.9.11"
 
 #define HLD_IDENT "@(#)hld " HLD_VERSION " - LP64 linker for HP-UX 11.23/IPF"
 
